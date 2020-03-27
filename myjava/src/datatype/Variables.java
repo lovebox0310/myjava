61d1173b07429a0e59874074f3cdd7dffe4a72a2
lovebox0310 <lovebox0310@gmail.com>
2020-03-27 12:24:33 +0900

    0327 오전 수업

--- myjava/src/datatype/Variables.java
+++
@@ -1,9 +0,0 @@
-package datatype;
-
-public class Variables {
-	public static void main(String[] args) {
-		System.out.println("변수에 대해서 배워보자");
-		System.out.println("sysout은 sout으로도 쓸 수 있다.");
-		
-	}
-}
